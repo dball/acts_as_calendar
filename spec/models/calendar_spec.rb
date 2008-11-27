0a799309caa248c74f140218d2ff3e1edc652a9b
Donald Ball <donald.ball@gmail.com>
2008-11-26 18:27:48 -0600

    removing extraneous requires

--- spec/models/calendar_spec.rb
+++ spec/models/calendar_spec.rb
@@ -1,6 +1,4 @@
-require File.dirname(__FILE__) + '/../factory'
 require File.dirname(__FILE__) + '/../spec_helper'
-require File.dirname(__FILE__) + '/../../../../../config/environment'
 
 describe Calendar do
   it "should have a date range" do
