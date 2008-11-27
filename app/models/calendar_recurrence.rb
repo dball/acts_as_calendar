f04ada716e42b876d6e6ab275ff8cd6efe526b39
Donald Ball <donald.ball@gmail.com>
2008-11-27 00:19:03 -0600

    correcting validation rule

--- app/models/calendar_recurrence.rb
+++ app/models/calendar_recurrence.rb
@@ -3,7 +3,7 @@ class CalendarRecurrence < ActiveRecord::Base
 
   validates_presence_of :calendar_event
   validates_inclusion_of :weekday, :in => 0..6, :allow_nil => true
-  validates_inclusion_of :monthday, :in => 0..31, :allow_nil => true
+  validates_inclusion_of :monthday, :in => 1..31, :allow_nil => true
   validates_inclusion_of :monthweek, :in => -1..4, :allow_nil => true
 
   validate :validate_pattern
