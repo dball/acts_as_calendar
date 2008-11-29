509a432ca73872caa98e37bf8c44e01e396cb9f0
Donald Ball <donald.ball@gmail.com>
2008-11-29 16:03:07 -0600

    testing associations, date population helper methods

--- spec/models/calendar_spec.rb
+++ spec/models/calendar_spec.rb
@@ -1,8 +1,33 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe Calendar do
-  it "should have a date range" do
-    calendar = Factory(:calendar)
-    calendar.should_not be_nil
+describe Calendar, "when empty" do
+  before(:each) do
+    @calendar = Factory(:calendar)
+  end
+
+  it "should have no dates" do
+    @calendar.dates.should == []
+    @calendar.dates.values.should == []
+  end
+
+  it "should have no events" do
+    @calendar.events.should == []
+  end
+
+  it "should fill with dates" do
+    dates = (Date.parse('2008-01-01') .. Date.parse('2008-01-02'))
+    @calendar.fill_dates(dates)
+    @calendar.dates.values.should == dates.to_a
+  end
+end
+
+describe Calendar, "when created for certain dates" do
+  before(:each) do
+    @dates = (Date.today .. 2.days.since(Date.today))
+    @calendar = Calendar.create_for_dates(@dates.first, @dates.last)
+  end
+
+  it "should have those dates" do
+    @calendar.dates.values.should == @dates.to_a
   end
 end
