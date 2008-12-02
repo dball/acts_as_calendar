7b551fe0edc64d0b7981bd56b74ee709f28a6d55
Donald Ball <donald.ball@gmail.com>
2008-12-01 18:40:59 -0600

    refactoring parse_specific_dates, parse_dates

--- spec/acts_as_calendar_spec.rb
+++ spec/acts_as_calendar_spec.rb
@@ -3,43 +3,51 @@ require 'acts_as_calendar'
 
 describe ActsAsCalendar do
   before(:all) do
-    class Test
+    class ExtendsCalendar
       extend ActsAsCalendar
     end
   end
 
-  describe "monthweek method" do
-    it "on first" do
-      Test.monthweek('first').should == 0
-    end
-    it "on last" do
-      Test.monthweek('last').should == -1
-    end
-  end
-
-  describe "parse weekly_dates method" do
+  describe "parse_dates method" do
     it "on Saturdays" do
-      Test.parse_weekly_dates('Saturdays').should == 
+      ExtendsCalendar.parse_dates('Saturdays').should == 
         { :weekday => 6, :monthweek => nil }
     end
 
     it "on Every Friday" do
-      Test.parse_weekly_dates('Every Friday').should ==
+      ExtendsCalendar.parse_dates('Every Friday').should ==
         { :weekday => 5, :monthweek => nil }
     end
 
     it "on every 3rd tuesday" do
-      Test.parse_weekly_dates('every 3rd tuesday').should ==
+      ExtendsCalendar.parse_dates('every 3rd tuesday').should ==
         { :weekday => 2, :monthweek => 2 }
     end
 
     it "on 2nd and 4th Fridays of the month" do
-      Test.parse_weekly_dates('2nd and 4th Fridays of the month').should ==
+      ExtendsCalendar.parse_dates('2nd and 4th Fridays of the month').should ==
         { :weekday => 5, :monthweek => [1, 3] }
     end
 
     it "on foo" do
-      Test.parse_weekly_dates('foo').should be_nil
+      ExtendsCalendar.parse_dates('foo').should be_nil
+    end
+
+    it "on 5/13/2008" do
+      ExtendsCalendar.parse_dates('5/13/2008').should ==
+        Chronic.parse('2008-05-13')
+    end
+
+    it "on 5/13, 5/15, 5/17" do
+      ExtendsCalendar.parse_dates('5/13/08, 5/15/08, 5/17/08').should == 
+        ["2008-05-13", "2008-05-15", "2008-05-17"].map do |value|
+          Chronic.parse(value)
+        end
+    end
+
+    it "on jan 1st - jan 31st" do
+      ExtendsCalendar.parse_dates('jan 1st - jan 31st').should ==
+        (Chronic.parse("jan 1st") .. Chronic.parse("jan 31st"))
     end
   end
 end
